445f63f1dc09fc5807aed57fb3a74fa436f73c89
omertztoya <omersr9@gmail.com>
2023-12-25 19:31:28 +0200

    add a skills section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import NavBar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
+import Skills from "./components/Skills/skills";
+
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Intro/>
+      <Skills/>
+
+     
     </div>
   );
 }
---
+++ src/components/Skills/skills.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./skills.css";
+
+
+const Skills = () => {
+    return (   
+
+<section id="skills" class="skills section-bg">
+  <div class="container">
+    <div class="section-title">
+   <p className="mySkillsPara">My skills</p>
+ <p className="skillspara">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/> Nulla ultricies arcu ex, id bibendum diam imperdiet et.</p>
+    </div>
+
+    <div class="row skills-content">
+      <div class="marquee">
+        <div class="marquee-content slick-initialized slick-slider">
+          <div class="slick-list draggable">
+            <div class="slick-track">
+            <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img className="imgSkills" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a>
+ <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img className="imgSkills" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a>
+  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img className="imgSkills"  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a>
+ 
+ <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img  className="imgSkills" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40"/> </a> 
+ <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img className="imgSkills" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> 
+ <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img  className="imgSkills" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/> </a> <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img className="imgSkills" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a>
+ <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer"> <img className="imgSkills" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width="40" height="40"/> </a> <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img className="imgSkills" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a> <a href="https://www.w3schools.com/cs/" target="_blank" rel="noreferrer"> <img className="imgSkills" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" alt="csharp" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img className="imgSkills" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> <a href="https://developer.apple.com/swift/" target="_blank" rel="noreferrer"> <img  className="imgSkills" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/swift/swift-original.svg" alt="swift" width="40" height="40"/> </a>
+
+            
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+        );
+        
+        
+}
+
+export default Skills;
